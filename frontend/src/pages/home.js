faae0665621808ea5ee1b547d7342102f7e2ed2a
viniciusr01 <viniciusr02@yahoo.com.br>
2023-06-02 13:02:56 -0300

    Add new features in frontend

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,41 +3,60 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import logo from '../images/logo.png'
 
+import React, { useState, useEffect } from 'react';
+
+
 
 function Home(){
 
+    const [usuarios, setUsuarios] = useState([])
+    
+    useEffect(() => {
+        
+        fetch('http://localhost:5000/pull/users',{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+              console.log(data)
+            })
+            .catch((error)=> console.log(error))
 
+    }, [])
 
     return (
         
-        <div class="container-fluid">
-                <div class="row flex-nowrap">
-                        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary">
-                                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                                        <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                                                <span class="fs-5 d-none d-sm-inline"></span>
+        <div className="container-fluid">
+                <div className="row flex-nowrap">
+                        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary">
+                                <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                                        <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                                                <span className="fs-5 d-none d-sm-inline"></span>
                                                 
                                         </a>
-                                        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                                        <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                                                 
                                                 <br></br>
-                                                <img class='logo' src={logo}></img>
+                                                <img className='logo' alt="Logo do Geolabor" src={logo}></img>
                                                 
                                                 <br></br>
-                                                <li class="nav-item">
-                                                        <a href="#" class="nav-link align-middle px-0 text-white ">
-                                                                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                                                <li className="nav-item">
+                                                        <a href="/" className="nav-link align-middle px-0 text-white ">
+                                                                <i className="fs-4 bi-house"></i> <span className="ms-1 d-none d-sm-inline">Home</span>
                                                         </a>
                                                 </li>
                                                 <li>
-                                                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white ">
-                                                                <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Usuários</span> </a>
-                                                                <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                                                                <li class="w-100">
-                                                                        <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline text-white">Item 1</span> </a>
+                                                        <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle text-white ">
+                                                                <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Usuários</span> </a>
+                                                                <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                                                <li className="w-100">
+                                                                        <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline text-white">Item 1</span> </a>
                                                                 </li>
                                                                 <li>
-                                                                        <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline text-white">Item 2</span></a>
+                                                                        <a href="/" className="nav-link px-0"> <span className="d-none d-sm-inline text-white">Item 2</span></a>
                                                                 </li>
                                                         </ul>
                                                 </li>
@@ -46,24 +65,24 @@ function Home(){
                                         </ul>
                                 
                                         {/*
-                                        <div class="dropdown pb-4">
-                                                <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <div className="dropdown pb-4">
+                                                <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                                                 
-                                                <span class="d-none d-sm-inline mx-1">User</span>
+                                                <span className="d-none d-sm-inline mx-1">User</span>
                                                 </a>
-                                                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                                                                <li><a class="dropdown-item" href="#">New project...</a></li>
-                                                                <li><a class="dropdown-item" href="#">Settings</a></li>
-                                                                <li><a class="dropdown-item" href="#">Profile</a></li>
+                                                        <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
+                                                                <li><a className="dropdown-item" href="#">New project...</a></li>
+                                                                <li><a className="dropdown-item" href="#">Settings</a></li>
+                                                                <li><a className="dropdown-item" href="#">Profile</a></li>
                                                                 <li></li>
-                                                                <li><a class="dropdown-item" href="#">Sign out</a></li>
+                                                                <li><a className="dropdown-item" href="#">Sign out</a></li>
                                                         </ul>
                                         </div>
                                          */}
                                 </div>
                         </div>
                 
-                                <div class="col py-3">
+                                <div className="col py-3">
                                         Content area...
                                 </div>
                 </div>
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+var cors = require('cors')
 require('dotenv').config()
 
 
 const app = express()
-
+app.use(cors())
 
 app.use(express.urlencoded({
         extended: true,
